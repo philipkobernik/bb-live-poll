3f336685aaa305d6ff169a6f19142a15ee9b9438
Philip Kobernik <ptk921@gmail.com>
2012-12-19 16:55:50 -0800

    Initial commit with modified Gemfile. Added haml-rails and rails-backbone.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BbLivePoll::Application.config.secret_token = '13b7d565872804c2f97706e10b5f017685292755203dbd945fbc89cfb498e2baeda51eaded70e48674d28b772e32c88e8e89ac2dd770997309ad58d95c3bbdac'
